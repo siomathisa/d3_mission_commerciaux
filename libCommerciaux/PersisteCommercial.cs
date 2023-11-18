a4ef43a7da24ef0fa329cd8583d130916db9be17
siomathisa <vidueira.mathis.sio@gmail.com>
2023-11-18 20:32:33 +0100

    test

---
+++ libCommerciaux/PersisteCommercial.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+namespace libCommerciaux
+{
+    [System.Serializable]
+    public class PersisteCommercial
+    {
+        public static void sauve(ServiceCommercial sc)
+        {
+            FileStream fichier = new FileStream("service.sr", FileMode.Create);
+            BinaryFormatter bf = new BinaryFormatter();
+            bf.Serialize(fichier, sc);
+            fichier.Close();
+        }
+        public static void charge()
+        {
+            FileStream fichier = new FileStream("service.sr", FileMode.Open);
+            BinaryFormatter bf = new BinaryFormatter();
+            bf.Deserialize(fichier);
+            fichier.Close();
+        }
+    }
+}
